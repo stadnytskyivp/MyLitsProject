7ac856bf86fac4cb1b7b50b82b226fcabe7eb886
stadnytskyivp <stadnytskyi.volodymyr@gmail.com>
2019-12-08 14:16:59 +0200

    homework 4 Zoo

--- src/homework/hw4/zoo/Worker.java
+++ src/homework/hw4/zoo/Worker.java
@@ -2,19 +2,29 @@ package homework.hw4.zoo;
 
 public class Worker {
 
+    private String Name;
     private String animal;
     private String food;
 
     public class CarlJonson extends Worker{
-
+        private String Name;
+        public CarlJonson(String name){ Name=name; }
+        @Override
+        public String getName() { return Name; }
     }
 
     public class AhmedAmir extends Worker{
-
+        private String Name;
+        public AhmedAmir(String name){ Name=name; }
+        @Override
+        public String getName() { return Name; }
     }
 
     public class AdolfHitzer extends Worker{
-
+        private String Name;
+        public AdolfHitzer(String name){ Name=name; }
+        @Override
+        public String getName() { return Name; }
     }
 
 
@@ -30,7 +40,7 @@ public class Worker {
         return food;
     }
 
-    public void setFood(String food) {
-        this.food = food;
-    }
+    public void takesFood(String food) { this.food = food; }
+
+    public String getName() { return Name; }
 }
--- src/homework/hw4/zoo/Zoo.java
+++ src/homework/hw4/zoo/Zoo.java
@@ -4,28 +4,60 @@ public class Zoo {
 
     public static void main(String[] args) {
 
-        Animal animal = new Animal();
-        Animal.Monkey monkey = animal.new Monkey();
-        Animal.Donkey donkey = animal.new Donkey();
-        Animal.Bear bear = animal.new Bear();
+        Zoo zoo = new Zoo();
+        zoo.openZoo();
+        zoo.feedAnimals();
+
+    }
+
+    public void openZoo(){
+        System.out.println("Zoo is opened:");
+
         Worker worker = new Worker();
-        Worker.AdolfHitzer adolf = worker.new AdolfHitzer();
-        Worker.AhmedAmir ahmed = worker.new AhmedAmir();
-        Worker.CarlJonson carl = worker.new CarlJonson();
+        Worker.AdolfHitzer adolf = worker.new AdolfHitzer("Adolf Hitzer");
+        Worker.AhmedAmir ahmed = worker.new AhmedAmir("Ahmed Amir");
+        Worker.CarlJonson carl = worker.new CarlJonson("Carl Jonson");
+        Animal animal = new Animal();
+        Animal.Monkey monkey = animal.new Monkey("monkey");
+        Animal.Donkey donkey = animal.new Donkey("donkey");
+        Animal.Bear bear = animal.new Bear("bear");
 
+        adolf.watchAfter(donkey.getAnimal());
+        carl.watchAfter(monkey.getAnimal());
+        ahmed.watchAfter(bear.getAnimal());
 
+        System.out.println(adolf.getName()+" will watch after "+adolf.getAnimal());
+        System.out.println(carl.getName()+" will watch after "+carl.getAnimal());
+        System.out.println(ahmed.getName()+" will watch after "+ahmed.getAnimal());
 
-        adolf.watchAfter("donkey");
-        adolf.setFood("grass");
+        System.out.println("");
+    }
+
+    public void feedAnimals(){
+        System.out.println("Time to feed animals:");
+
+        Worker worker = new Worker();
+        Worker.AdolfHitzer adolf = worker.new AdolfHitzer("Adolf Hitzer");
+        Worker.AhmedAmir ahmed = worker.new AhmedAmir("Ahmed Amir");
+        Worker.CarlJonson carl = worker.new CarlJonson("Carl Jonson");
+        Animal animal = new Animal();
+        Animal.Monkey monkey = animal.new Monkey("monkey");
+        Animal.Donkey donkey = animal.new Donkey("donkey");
+        Animal.Bear bear = animal.new Bear("bear");
+
+        adolf.takesFood("grass");
+        ahmed.takesFood("banana");
+        carl.takesFood("banana");
+
+        System.out.println(adolf.getName()+" takes "+adolf.feedAnimal());
+        System.out.println(ahmed.getName()+" takes "+ahmed.feedAnimal());
+        System.out.println(carl.getName()+" takes "+carl.feedAnimal());
+
+        System.out.println("");
+
         donkey.eat(adolf.feedAnimal());
-
-        carl.watchAfter("monkey");
-        carl.setFood("carrot");
-        monkey.eat(carl.feedAnimal());
-
-        ahmed.watchAfter("bear");
-        ahmed.setFood("meat");
         bear.eat(ahmed.feedAnimal());
+        monkey.eat(carl.feedAnimal());
 
     }
 
